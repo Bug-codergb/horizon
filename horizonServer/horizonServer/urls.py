5ff4b6938d26a366df114aafde73d1f38ab3ef0f
codergb <betweent@126.com>
2025-02-19 21:12:20 +0800

    虚拟环境

--- horizonServer/horizonServer/urls.py
+++ horizonServer/horizonServer/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from web import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("gb",views.init)
 ]
